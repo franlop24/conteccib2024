0a26a0c7ba8cca3fd0b7569fc83dea46ec49c640
Franlop24 <franlopbri@gmail.com>
2024-10-25 13:41:30 -0600

    05 Implements improvements in workshop App

--- congress/workshop/views.py
+++ congress/workshop/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 from .models import Workshop
 from .forms import WorkshopForm
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 
 class WorkshopListView(ListView):
     model = Workshop
     template_name = 'workshop/list.html'
     context_object_name = 'workshops'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = [workshop for workshop in queryset if workshop.available_seats > 0]
+        return queryset
 
 
 class WorkshopDetailView(DetailView):
@@ -17,7 +25,9 @@ class WorkshopDetailView(DetailView):
     context_object_name = 'workshop'
 
 
-class WorkshopCreateView(CreateView):
+class WorkshopCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    def test_func(self):
+        return self.request.user.is_staff
     model = Workshop
     form_class = WorkshopForm
     template_name = 'workshop/create.html'
@@ -26,7 +36,10 @@ class WorkshopCreateView(CreateView):
         return reverse_lazy('workshop:list')
 
 
-class WorkshopUpdateView(UpdateView):
+class WorkshopUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    def test_func(self):
+        return self.request.user.is_staff
     model = Workshop
+    form_class = WorkshopForm
     template_name = 'workshop/update.html'
-    fields = '__all__'
+    success_url = reverse_lazy('workshop:list')
