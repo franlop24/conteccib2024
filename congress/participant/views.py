f4537a5bdc88a3379eca525ea072ceca77e2426e
Franlop24 <franlopbri@gmail.com>
2024-10-26 12:48:41 -0600

    05 Implements Security in Apps Participant and Workshop

--- congress/participant/views.py
+++ congress/participant/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.views.generic import CreateView, ListView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 
 from .models import Participant
 from .forms import ParticipantCreateForm, SearchParticipantForm, ValidateParticipantForm
@@ -11,7 +14,14 @@ class ParticipantCreateView(CreateView):
     template_name = 'participant/participant_register.html'
     success_url = reverse_lazy('login')
 
-class ParticipantListView(ListView):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
+class ParticipantListView(LoginRequiredMixin, UserPassesTestMixin, ListView):
+    def test_func(self):
+        return self.request.user.is_staff
     model = Participant
     template_name = 'participant/participant_list.html'
     context_object_name = 'participants'
@@ -27,7 +37,9 @@ class ParticipantListView(ListView):
         context = super().get_context_data(**kwargs)
         context['form'] = SearchParticipantForm()
         return context
-    
+
+@staff_member_required
+@login_required
 def validate_participant(request, pk):
     form = ValidateParticipantForm()
     participant = get_object_or_404(Participant, pk=pk)
