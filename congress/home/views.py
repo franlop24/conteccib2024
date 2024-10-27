cd5150f62bce44c0d1e1a87d0bae81d09b314524
Franlop24 <franlopbri@gmail.com>
2024-10-27 12:18:31 -0600

    07 Implements Security in Home App and impleents media queries

--- congress/home/views.py
+++ congress/home/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django import http
 
 # Create your views here.
 def home(request):
@@ -10,8 +12,16 @@ def home(request):
             return redirect('home:participant')
     return render(request, 'home/home.html')
 
+@login_required
 def admin(request):
-    return render(request, 'home/administrator.html')
+    if request.user.is_superuser:
+        return render(request, 'home/administrator.html')
+    else:
+        raise http.Http404
 
+@login_required
 def participant(request):
-    return render(request, 'home/participant.html')
+    if hasattr(request.user, 'participant'):
+        return render(request, 'home/participant.html')
+    else:
+        raise http.Http404
